41de909503640adcb480995ae123a00de45497b7
Nick <nsagias@gmail.com>
2021-10-06 18:18:43 -0400

    Fix strech and alignment bug

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,7 @@
-console.log('test composer char counter works')
+$(document).ready(function() {
+  console.log('My document is ready')
+  $("#btn").on('click', function(event) {
+    console.log(this); //The this keyword is a reference to the button
+    event.preventDefault();
+  });
+});
