272db110eaacf8c96ec7d09581118ba23268b896
Nick <nsagias@gmail.com>
2021-10-06 21:09:44 -0400

    Remove old notes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,5 @@
-// $(document).ready(function() {
-//   console.log('My document is ready')
-//   $("#btn").on('click', function(event) {
-//     console.log(this); //The this keyword is a reference to the button
-//     event.preventDefault();
-//   });
-// });
-
 // "tweet-text"
-// Textarea
-// $(() => {
-//   $("#tweet-text").keyup( () => { 
-//     console.log($("#tweet-text").val().length);
-//   });
-// });
+// tweet charater counter
 
 $(() => {
   $("#tweet-text").keyup( () => { 
