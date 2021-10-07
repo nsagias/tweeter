1ebb6b995809213b1b56ea9452b4dae3f1eb2021
Nick <nsagias@gmail.com>
2021-10-06 20:27:03 -0400

    Select jQuery code to use

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
-  console.log('My document is ready')
-  $("#btn").on('click', function(event) {
-    console.log(this); //The this keyword is a reference to the button
-    event.preventDefault();
-  });
+// $(document).ready(function() {
+//   console.log('My document is ready')
+//   $("#btn").on('click', function(event) {
+//     console.log(this); //The this keyword is a reference to the button
+//     event.preventDefault();
+//   });
+// });
+
+// "tweet-text"
+// Textarea
+$(() => {
+  $("#tweet-text").keyup( () => { console.log("test")})
 });
