671d773a1484e83a69399dd8798fa495631c2b28
Nick <nsagias@gmail.com>
2021-10-07 23:13:00 -0400

    refactor tweet in its tag and elements, not combined

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -22,40 +22,39 @@ const tweetData = {
 
 const createTweetElement  = (tweetData) => {
 
-          // user.name 
-          // user.avatars 
-        // <div><img src="https://i.imgur.com/73hZDYK.png"></div>
-        const $userAvatars = $('<div>').text(`<img src="https://i.imgur.com/73hZDYK.png"></img>`); 
-        // <div>Newton</div>
-        const $userName = $('<div>').text(`Newton`);
+  // user.avatars  
+  // <div><img src="https://i.imgur.com/73hZDYK.png"></div>
+  const $userAvatars = $('<div>').text(`<img src=${"https://i.imgur.com/73hZDYK.png"}></img>`); 
+  
+  // user.name 
+  // <div>Newton</div>
+  const $userName = $('<div>').text(`Newton`);
 
-          // user.handle   
-        const $userHandle = $('<div>').text(`@SirIsaac`);
+    // user.handle   
+  const $userHandle = $('<div>').text(`@SirIsaac`);
 
-          content.text 
-          //  <div id="content">
-          //    <p >"If I have seen further it is by standing on the shoulders of giants"</p>
-          //  </div>
-          const $tweetContent = $('<p>').text(`If I have seen further it is by standing on the shoulders of giants`);
-        // <div id="tweet-footer">
-              created_at // <div>
-        //   <div>10 days ago</div>
-        //     <div class="tweet-footer-right">
-        //     <div><i class="fas fa-flag"></i></div>
-        //     <div><i class="fas fa-retweet"></i></div>
-        //     <div><i class="fas fa-heart"></i><sup>1</sup></div>
-        //   </div>
-        // </div>
-   
-        const $article = $('article').addClass("tweet");
-          const $header = $('header').addClass("tweet-header");
-            const $tweetHeaderLeft = $('div').addClass("tweet-header-left");
-            const $tweetHeaderRight = $('div').addClass("tweet-header-right");
-            const $content = $('div').addClass("content");
-          const $footer = $('div').addClass("tweet-footer");
-            const $tweetFooterLeft = $('div').addClass("tweet-footer-left");
-            const $tweetFooterRight = $('div').addClass("tweet-footer-right");
-         // end
+    // content.text 
+  const $tweetContent = $('<p>').text(`If I have seen further it is by standing on the shoulders of giants`);
+  
+  // <div id="tweet-footer">
+  // created_at
+  const $createdAt = $('<div>').text(`10 days ago`);
+
+  // icons
+  const $flagIcon = $('<div>').text('<i class="fas fa-flag"></i>');
+  const $retweetIcon = $('<div>').text('<i class="fas fa-retweet></i>');
+  const $heartIcon = $('<div>').text(`i class="fas fa-heart"></i><sup>${1}</sup>`);
+  
+  // article 
+  const $article = $('article').addClass("tweet");
+    const $header = $('header').addClass("tweet-header");
+      const $tweetHeaderLeft = $('div').addClass("tweet-header-left");
+      const $tweetHeaderRight = $('div').addClass("tweet-header-right");
+      const $content = $('div').addClass("content");
+    const $footer = $('div').addClass("tweet-footer");
+      const $tweetFooterLeft = $('div').addClass("tweet-footer-left");
+      const $tweetFooterRight = $('div').addClass("tweet-footer-right");
+    // end
 
 
   return tweetData;
