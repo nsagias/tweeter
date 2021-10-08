b9a62c04a60813d089171c9ec047f4cba15142be
Nick <nsagias@gmail.com>
2021-10-08 17:32:37 -0400

    Undo previous changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,78 +27,81 @@ $( () => {
         console.log(`There was an error: ${err}`);
       }
     });
-}); 
 
  
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Seinfeld",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@masterOfMyDomain" },
-    "content": {
-      "text": "I'm out!"
-    },
-    "created_at": 1461113959089
-  }
-]
 
 
-const createTweetElement  = (tweetData) => {
-const userName = tweetData.user.name;
-const userAvatars = tweetData.user.avatars;
-const userHandle = tweetData.user.handle;
-const tweetContent = tweetData.content.text;
-const createdAt = tweetData.created_at;
 
-let $tweet = `<article class="tweet">
-              <header class="tweet-header">
-                <div class="tweet-header-left">
-                  <div><img src=${userAvatars}></div>
-                  <div>${userName}</div>
+   
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Seinfeld",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@masterOfMyDomain" },
+      "content": {
+        "text": "I'm out!"
+      },
+      "created_at": 1461113959089
+    }
+  ];
+
+
+  const createTweetElement  = (tweetData) => {
+    const userName = tweetData.user.name;
+    const userAvatars = tweetData.user.avatars;
+    const userHandle = tweetData.user.handle;
+    const tweetContent = tweetData.content.text;
+    const createdAt = tweetData.created_at;
+
+    let $tweet = `<article class="tweet">
+                  <header class="tweet-header">
+                    <div class="tweet-header-left">
+                      <div><img src=${userAvatars}></div>
+                      <div>${userName}</div>
+                    </div>
+                    <div class="tweet-header-right">
+                      <div>${userHandle}</div>
+                    </div>
+                </header>
+                <div class="content">
+                  <p>${tweetContent}</p>
                 </div>
-                <div class="tweet-header-right">
-                  <div>${userHandle}</div>
+                  <div class="tweet-footer">
+                    <div class="tweet-footer-left">
+                      <div>${timeago.format(createdAt)}</div>
+                    </div>
+                    <div class="tweet-footer-right">
+                      <div><i class="fas fa-flag"></i></div>
+                      <div><i class="fas fa-retweet"></i></div>
+                      <div><i class="fas fa-heart"></i><sup>${1}</sup></div>
+                    </div>
                 </div>
-            </header>
-            <div class="content">
-              <p>${tweetContent}</p>
-            </div>
-              <div class="tweet-footer">
-                <div class="tweet-footer-left">
-                  <div>${timeago.format(createdAt)}</div>
-                </div>
-                <div class="tweet-footer-right">
-                  <div><i class="fas fa-flag"></i></div>
-                  <div><i class="fas fa-retweet"></i></div>
-                  <div><i class="fas fa-heart"></i><sup>${1}</sup></div>
-                </div>
-            </div>
-            </article>`;
-return $tweet;
-};
+                </article>`;
+    return $tweet;
+  };
 
 // const renderTweets = function(tweets) {
 //   $('#tweets-container').empty();
@@ -112,13 +115,15 @@ return $tweet;
 // renderTweets(data);
 
 
-const $form = $("#form-new-tweet");
-$form.submit(function(event) {
-  event.preventDefault();
-  const serializedData = $(this).serialize();
-  console.log('form was submitted');
+  const $form = $("#form-new-tweet");
+  $form.submit(function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+    console.log('form was submitted');
 
-  $.post("/tweets", serializedData, (resp) => {
+    $.post("/tweets", serializedData, (resp) => {
 
+    });
   });
-});
+
+}); 
