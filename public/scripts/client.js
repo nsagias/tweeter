b0059ed7e97389023b162a294a287d10ff8ee5a5
Nick <nsagias@gmail.com>
2021-10-08 00:32:43 -0400

    clean code, jquery code doesn't work

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,64 +20,41 @@ const tweetData = {
   "created_at": 1461116232227
 }
 
-const createTweetElement  = (tweetData) => {
+// const createTweetElement  = (tweetData) => {
 
   // user.avatars  
   // <div><img src="https://i.imgur.com/73hZDYK.png"></div>
-  const $userAvatars = $('<div>').text(`<img src=${"https://i.imgur.com/73hZDYK.png"}></img>`); 
-  
-  // user.name 
-  // <div>Newton</div>
+  const $userAvatars = $('<div>').prepend(`<img src="https://i.imgur.com/73hZDYK.png"></img>`); 
+
+  // // // user.name 
+  // // // <div>Newton</div>
   const $userName = $('<div>').text(`Newton`);
 
-    // user.handle   
+  // //   // user.handle   
   const $userHandle = $('<div>').text(`@SirIsaac`);
 
-    // content.text 
+  // //   // content.text 
   const $tweetContent = $('<p>').text(`If I have seen further it is by standing on the shoulders of giants`);
   
-  // <div id="tweet-footer">
-  // created_at
+  // // // <div id="tweet-footer">
+  // // // created_at
   const $createdAt = $('<div>').text(`10 days ago`);
 
-  // icons
+  // // // icons
   const $flagIcon = $('<div>').text('<i class="fas fa-flag"></i>');
   const $retweetIcon = $('<div>').text('<i class="fas fa-retweet></i>');
   const $heartIcon = $('<div>').text(`i class="fas fa-heart"></i><sup>${1}</sup>`);
   
-  // article 
-  const $article = $('article').addClass("tweet");
-    const $header = $('header').addClass("tweet-header");
-      const $tweetHeaderLeft = $('div').addClass("tweet-header-left");
-      const $tweetHeaderRight = $('div').addClass("tweet-header-right");
-    const $content = $('div').addClass("content");
-    const $footer = $('div').addClass("tweet-footer");
-      const $tweetFooterLeft = $('div').addClass("tweet-footer-left");
-      const $tweetFooterRight = $('div').addClass("tweet-footer-right");
+  // // article 
+  // const $article = $('article').addClass("tweet");
+  //   const $header = $('header').addClass("tweet-header");
+  //     const $tweetHeaderLeft = $('div').addClass("tweet-header-left");
+  //     const $tweetHeaderRight = $('div').addClass("tweet-header-right");
+  //   const $content = $('div').addClass("content");
+  //   const $footer = $('div').addClass("tweet-footer");
+  //     const $tweetFooterLeft = $('div').addClass("tweet-footer-left");
+  //     const $tweetFooterRight = $('div').addClass("tweet-footer-right");
   
-  // header
-  $tweetHeaderLeft.append($userAvatars, $userName);
-  $tweetHeaderRight.append($userHandle);
-  
-  // content
-  $content.append($tweetContent);
-  
-  // footer   
-  $tweetFooterLeft.append($createdAt);
-  $tweetFooterRight.append($flagIcon, $retweetIcon,$heartIcon);
-  
-  // article
-  $header.append($tweetHeaderLeft, $tweetHeaderRight);
-  $footer.append($tweetFooterLeft, $tweetFooterRight);
-  $article.append($header, $content, $footer);
-   
-
-  return tweetData;
-}; 
-
-
-
-
 
 // const $tweet = createTweetElement(tweetData);
 
@@ -86,5 +63,6 @@ const createTweetElement  = (tweetData) => {
 
 // works
 // const $tweet = $('article').text('hello world')
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); 
+console.log($article); // to see what it looks like
+// $('#tweets-container').append($userAvatars, $userName, $userHandle); 
+$('#tweets-container').append($article); 
