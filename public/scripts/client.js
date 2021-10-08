c9f91e3c36c88ee7cedf22adede7b7e678ef4e69
Nick <nsagias@gmail.com>
2021-10-07 22:35:39 -0400

    Saving Code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,37 +6,6 @@
 
 // const $tweet = $(`<article class="tweet">Hello world</article>`);
 
-const createTweetElement  = (tweetData) => {
-  // extract info from object
-  // and return object with html markup from design
-  // <article class="tweet">
-  //   <header class="tweet-header"></header>
-          user.name 
-          user.avatars 
-      //  <div class="tweet-header-left">
-      //     <div><img src="https://i.imgur.com/73hZDYK.png"></div>
-      //     <div>Newton</div>
-      //  </div>
-          user.handle // <div class="tweet-header-right">@SirIsaac</div>
-          content.text // <<div id="content"> <p>
-          //  <div id="content">
-          //    <p >"If I have seen further it is by standing on the shoulders of giants"</p>
-          //  </div>
-        // <div id="tweet-footer">
-              created_at // <div>
-        //   <div>10 days ago</div>
-        //     <div class="tweet-footer-right">
-        //     <div><i class="fas fa-flag"></i></div>
-        //     <div><i class="fas fa-retweet"></i></div>
-        //     <div><i class="fas fa-heart"></i><sup>1</sup></div>
-        //   </div>
-        // </div>
-   
-  return tweetData;
-}; 
-
-// console.log('this is my tweet',$tweet);
-// $('#tweets-container').append($tweet);
 
 // Test / driver code (temporary). Eventually will get this from the server.
 const tweetData = {
@@ -51,8 +20,82 @@ const tweetData = {
   "created_at": 1461116232227
 }
 
-const $tweet = createTweetElement(tweetData);
+const createTweetElement  = (tweetData) => {
+
+  
+  // <article class="tweet">
+  // const $header = $('header').addClass("tweet-header")
+  //   <header class="tweet-header"></header>
+          user.name 
+          user.avatars 
+      // const $tweet = $('div').addClass("tweet-header-left")
+      //  <div class="tweet-header-left">
+      //     <div><img src="https://i.imgur.com/73hZDYK.png"></div>
+      //     <div>Newton</div>
+      //  </div>
+      // const $tweet = $('div').addClass("tweet-header-right")
+         // const $tweet = $('div').addClass("tweet-header-left")
+          user.handle // <div class="tweet-header-right">@SirIsaac</div>
+          content.text // <div id="content"> <p>
+          //  <div id="content">
+          //    <p >"If I have seen further it is by standing on the shoulders of giants"</p>
+          //  </div>
+        // <div id="tweet-footer">
+              created_at // <div>
+        //   <div>10 days ago</div>
+        //     <div class="tweet-footer-right">
+        //     <div><i class="fas fa-flag"></i></div>
+        //     <div><i class="fas fa-retweet"></i></div>
+        //     <div><i class="fas fa-heart"></i><sup>1</sup></div>
+        //   </div>
+        // </div>
+   
+        const $article = $('article').addClass("tweet");
+          const $header = $('header').addClass("tweet-header");
+            const $tweetHeaderLeft = $('div').addClass("tweet-header-left");
+            const $tweetHeaderRight = $('div').addClass("tweet-header-right");
+            const $content = $('div').addClass("content");
+          const $footer = $('div').addClass("tweet-footer");
+            const $tweetFooterLeft = $('div').addClass("tweet-footer-left");
+            const $tweetFooterRight = $('div').addClass("tweet-footer-right");
+
+
+
+  return tweetData;
+}; 
+
+
+{/* <article class="tweet">
+<header class="tweet-header">
+  <div class="tweet-header-left">
+    <div><img src="https://i.imgur.com/73hZDYK.png"></div>
+    <div>Newton</div>
+  </div>
+  <div class="tweet-header-right">@SirIsaac</div>
+</header>
+<div id="content">
+ <p>"If I have seen further it is by standing on the shoulders of giants"</p>
+</div>
+<div id="tweet-footer">
+  <div>10 days ago</div>
+  <div class="tweet-footer-right">
+    <div><i class="fas fa-flag"></i></div>
+    <div><i class="fas fa-retweet"></i></div>
+    <div><i class="fas fa-heart"></i><sup>1</sup></div>
+  </div>
+</div>
+</article> */}
+// console.log('this is my tweet',$tweet);
+// $('#tweets-container').append($tweet);
+
+
+
+// const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
+// const $tweet = $('article').addClass("tweet")
+
+// works
+// const $tweet = $('article').text('hello world')
 console.log($tweet); // to see what it looks like
 $('#tweets-container').append($tweet); 
