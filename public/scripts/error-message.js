9c3eb39796dcab03145216fb5436ba24fb7b292a
Nick <nsagias@gmail.com>
2021-10-11 03:17:12 -0400

    Error message css sheet

--- public/scripts/error-message.js
+++ public/scripts/error-message.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//   $( "#clickme" ).click(function() {
+//     $( "#book" ).slideDown( "slow", function() {
+//       // Animation complete.
+//     });
+//   });
+// });
 
+$('#error' ).click(function () {
+  if ( $( "div" ).first().is( ":hidden" ) ) {
+    $( "div" ).slideDown( "slow" );
+  } else {
+    $( "div" ).hide();
+  }
 });
