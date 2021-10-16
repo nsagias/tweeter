e5682102d23d37a62eac84cbbef46751f82578e9
Nick <nsagias@gmail.com>
2021-10-16 17:40:18 -0400

    Refactor double arrow function

--- public/scripts/double-arrow-focus.js
+++ public/scripts/double-arrow-focus.js
@@ -1,10 +1,12 @@
 /**
  * Double arrow scrolls and focus on textarea
  */
-$(document).ready(function () {
+$().ready(doubleArrow);
+
+const doubleArrow = () => {
   $(".double-arrow").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "fast");
     $("#tweet-text").focus();
     return false;
   });
-});
+};
